31b3f27f286e6dd2d17c6fc34a5453a61235041e
AAnirudh07 <86918353+AAnirudh07@users.noreply.github.com>
2022-04-20 18:06:49 +0530

    add reinforcement learning environment

---
+++ My Code/reinforcement_learning_env.py
@@ -0,0 +1,113 @@
+import glob
+import os
+import sys
+import random 
+import time
+import math
+from cv2 import transform
+import numpy as np
+import cv2
+
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+import carla
+
+SHOW_PREVIEW = False
+IM_WIDTH = 640
+IM_HEIGHT = 480
+SECONDS_PER_EPISODE = 10
+
+class CarEnv:
+    SHOW_CAM = SHOW_PREVIEW
+    STEER_AMT = 1.0
+    im_width = IM_WIDTH
+    im_height = IM_HEIGHT
+    front_cam = None
+
+    def __init__(self):
+        self.client = carla.Client("localhost",2000)
+        self.client.set_timeout(5.0)
+        self.world = self.client.get_world()
+        self.blueprint_library = self.world.get_blueprint_library()
+        self.model3 = self.blueprint_library.filter("model3")[0]
+    
+    def reset(self):
+        self.collision_hist = []
+        self.actor_list = []
+
+        self.transform = random.choice(self.world.get_map().get_spawn_points())
+        self.vehicle = self.world.spawn_actor(self.model3, self.transform)
+        self.actor_list.append(self.vehicle)
+
+        self.rgb_cam = self.blueprint_library.find("sensor.camera.rgb")
+        self.rgb_cam.set_attribute("img_size_x", f"{self.im_width}")
+        self.rgb_cam.set_attribute("img_size_y", f"{self.im_height}")
+        self.rgb_cam.set_attribute("fov", f"100")
+
+        transform = carla.Transform(carla.Location(x=2.5,z=0.7))
+        self.sensor = self.world.spawn_actor(self.rgb_cam, transform, attach_to=self.vehicle)
+        self.actor_list.append(self.sensor)
+
+        self.sensor.listen(lambda data: self.process_img(data))
+
+        self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, brake=0.0))
+        time.sleep(4)
+
+        colsensor = self.blueprint_library.find("sensor.other.collision")
+        self.colsensor = self.world.spawn_actor(colsensor, transform, attach_to=self.vehicle)
+        self.actor_list.append(self.colsensor)
+
+        self.colsensor.listen(lambda event: self.collision_data(event))
+
+
+        while self.front_cam is None:
+            time.sleep(0.01)
+        
+        self.episode_start = time.time()
+        self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, brake=0.0))
+
+        return self.front_cam
+
+    def collision_data(self,event):
+        self.collision_hist.append(event)
+    
+    def process_img(self, data):
+        i = np.array(data.raw_data)
+        i2 = i.reshape((self.im_height,self.im_width, 4))
+        i3 = i2[:, :, :3]
+        if self.SHOW_CAM:
+            cv2.imshow("",i3)
+            cv2.waitKey(1)
+        self.front_cam = i3
+    
+    def step(self, action):
+        if action == 0:
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=-1*self.STEER_AMT))
+        elif action == 1:
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=0))
+        elif action == 2:
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=1*self.STEER_AMT))
+
+        v = self.vehicle.get_velocity()
+        kmh = int(3.6*math.sqrt(v.x**2 + v.y**2 + v.z**2))
+
+        if len(self.collision_hist) != 0:
+            done = True
+            reward = -200
+        elif kmh < 50:
+            done = False
+            reward = -1
+        else:
+            done = False
+            reward = 1
+
+        if self.episode_start + SECONDS_PER_EPISODE < time.time():
+            done = True
+
+        return self.front_cam, reward, done, None
